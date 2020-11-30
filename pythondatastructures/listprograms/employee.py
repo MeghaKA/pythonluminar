2de2d370de9337519ce191563eaef83fa2731f24
meghaka <meghaka1998@gmail.com>
2020-11-30 21:16:21 +0530

    list programs employee 30/11/20

--- pythondatastructures/listprograms/employee.py
+++ pythondatastructures/listprograms/employee.py
@@ -1,7 +1,7 @@
-employees=[[1001,"ajay","qa",15000],
-           [1002,"vijay","developer",25000],
-           [1003,"arun","ba",15000],
-           [1004,"amal","developer",30000]]
+#employees=[[1001,"ajay","qa",15000],
+#           [1002,"vijay","developer",25000],
+ #          [1003,"arun","ba",15000],
+  #         [1004,"amal","developer",30000]]
 #print all employee id
 
 #find total of salary
@@ -10,21 +10,36 @@ employees=[[1001,"ajay","qa",15000],
 #find how  many members working as developer
 
 #findtotal  of salary group by designation
-qasum=0
-dsum=0
-for employee in employees:
-    if(employee[2]=="qa"):
-        qasum+=employee[3]
-    elif(employee[2]=="developer"):
-        dsum+=employee[3]
-print(dsum)
-print(qasum)
-
+#qasum=0
+#dsum=0
+#for employee in employees:
+ #   if(employee[2]=="qa"):
+  #      qasum+=employee[3]
+   # elif(employee[2]=="developer"):
+    #    dsum+=employee[3]
+#print(dsum)
+#print(qasum)
 
 employees=[[1001,"ajay","qa",1981,2003],
-           [1002,"vijay","developer",1992,2008],
-           [1003,"arun","ba",2009,2014],
-           [1004,"amal","developer",1987,1989]]
+          [1002,"vijay","developer",1992,2008],
+          [1003,"arun","ba",2009,2014],
+         [1004,"amal","developer",1987,1989]]
+
+for employee in employees:
+    print(employee[2])
+
+for employee in employees:
+    if employee[2]=='developer':
+       print(employee)
+
+for employee in employees:
+    if employee[3]<=1990:
+        print(employee)
+
+for employee in employees:
+    if employee[4]-employee[3]>9:
+        print(employee)
+
 
 #print all employee designation
 #print  all employee whose designation=developer
